0e7b7af9524e27169dc726f696e10c4826e09bd4
jonas@work <j_mens07@uni-muenster.de>
2023-11-14 17:48:28 +0100

    time stuff memory

--- scripts/time_dep_noise.py
+++ scripts/time_dep_noise.py
@@ -10,21 +10,21 @@ import multiprocessing
 if __name__ == '__main__':
 
     N_processes         = 10
-    N_electrodes        = 8
-    N_voltages          = 2500
-    max_time            = 10e-4
-    time_steps          = np.linspace(0,max_time,N_voltages)
+    N_electrodes        = 2
+    N_voltages          = 5000
+    step_size           = 1e-8
+    time_steps          = np.cumsum(np.repeat(step_size, N_voltages))
+    amplitude           = 0.2
 
-    def parallel_code(thread, N_voltages, time_steps, N_electrodes):
+    def parallel_code(thread, N_voltages, time_steps, amplitude, N_electrodes):
 
         np.random.seed(thread)
 
         for inner_stat in range(5):
 
-
-            N                           = 5
-            min_val                     = -1
-            max_val                     = 1
+            N                           = 9
+            min_val                     = -amplitude
+            max_val                     = amplitude
             data                        = np.cumsum(np.random.normal(0,1,N_voltages))
             data_s                      = ((data - np.min(data))/(np.max(data)-np.min(data)))*(max_val - min_val) + min_val  
             voltages_values             = data_s
@@ -32,12 +32,12 @@ if __name__ == '__main__':
             topology_parameter["Nx"]    = N
             topology_parameter["Ny"]    = N
             topology_parameter["Nz"]    = 1
-            topology_parameter["e_pos"] = [[0,0,0], [int((N-1)/2),0,0], [N-1,0,0], [0,int((N-1)/2),0],
-                                           [0,N-1,0], [N-1,int((N)/2),0], [int((N)/2),(N-1),0], [N-1,N-1,0]]
+            # topology_parameter["e_pos"] = [[0,0,0], [int((N-1)/2),0,0], [N-1,0,0], [0,int((N-1)/2),0],
+            #                                [0,N-1,0], [N-1,int((N)/2),0], [int((N)/2),(N-1),0], [N-1,N-1,0]]
+            topology_parameter["e_pos"] = [[0,0,0], [N-1,N-1,0]]
 
             target_electrode    = len(topology_parameter["e_pos"]) - 1
-            # folder              = "/home/jonas/phd/NanoNets/test_runs/time_run/data/noise/"#"/scratch/tmp/j_mens07/data/system_size/"
-            folder              = "/mnt/c/Users/jonas/Desktop/phd/NanoNets/test_runs/time_run/data/noise/"#"/scratch/tmp/j_mens07/data/system_size/"
+            folder              = "/home/jonas/phd/NanoNets/test_runs/time_run/data/noise/"
             voltages            = pd.DataFrame(np.zeros((N_voltages, N_electrodes+1)))
             voltages.iloc[:,0]  = voltages_values
 
@@ -47,5 +47,5 @@ if __name__ == '__main__':
     
     for i in range(N_processes):
 
-        process = multiprocessing.Process(target=parallel_code, args=(i, N_voltages, time_steps, N_electrodes))
+        process = multiprocessing.Process(target=parallel_code, args=(i, N_voltages, time_steps, amplitude, N_electrodes))
         process.start()

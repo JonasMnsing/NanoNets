3145ad70c162b6bc19a560186c33bb378326d086
Jonas Mensing <jonasmensing@posteo.de>
2023-11-14 10:44:01 +0100

    time

--- scripts/time_dep.py
+++ scripts/time_dep.py
@@ -3,52 +3,57 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("/home/jonas/phd/NanoNets/src/")
-# sys.path.append("/mnt/c/Users/jonas/Desktop/phd/NanoNets/src/")
+sys.path.append("/mnt/c/Users/jonas/Desktop/phd/NanoNets/src/")
 import model
 import multiprocessing
 
 if __name__ == '__main__':
 
     N_processes         = 10
-    N_electrodes        = 8
-    N_voltages          = 2500
-    max_time            = 10e-4
-    frequency           = 0.1e5
-    time_steps          = np.linspace(0,max_time,N_voltages)
-    amplitude           = 0.1
+    N_electrodes        = 2
+    N_voltages          = 5000
+    # frequency           = 2e5
+    step_size           = 1e-8
+    time_steps          = np.cumsum(np.repeat(step_size, N_voltages))
+    amplitude           = 0.2
 
-    def parallel_code(thread, N_voltages, frequency, time_steps, amplitude, N_electrodes):
+    def parallel_code(thread, N_voltages, time_steps, amplitude, N_electrodes):
 
-        for inner_stat in range(10): 
-            N                           = 7
-            voltages_values             = amplitude*np.cos(2*np.pi*frequency*time_steps)
-            topology_parameter          = {}
-            topology_parameter["Nx"]    = N
-            topology_parameter["Ny"]    = N
-            topology_parameter["Nz"]    = 1
-            topology_parameter["e_pos"] = [[0,0,0], [int((N-1)/2),0,0], [N-1,0,0], [0,int((N-1)/2),0], [0,N-1,0], [N-1,int((N)/2),0], [int((N)/2),(N-1),0], [N-1,N-1,0]]
+        for frequency in [7e5,8e5,9e5,10e5]:
 
-            target_electrode    = len(topology_parameter["e_pos"]) - 1
-            folder              = f"/home/jonas/phd/NanoNets/test_runs/time_run/data/periodic/dis_f_01_"#"/scratch/tmp/j_mens07/data/system_size/"
-            voltages            = pd.DataFrame(np.zeros((N_voltages, N_electrodes+1)))
-            voltages.iloc[:,0]  = voltages_values
-            
-            np_info = {
-                "eps_r"         : 2.6,
-                "eps_s"         : 3.9,
-                "mean_radius"   : 10.0,
-                "std_radius"    : 5.0,
-                "np_distance"   : 1.0
-            }
+            for inner_stat in range(5):
 
-            sim_class   = model.simulation(voltages.values)
-            sim_class.init_cubic(folder, topology_parameter, add_to_path=f"_{thread}_{inner_stat}", np_info=np_info)
-            sim_class.run_var_voltages(target_electrode, time_steps, save_th=1)
-            # model.time_simulation(target_electrode, time_steps, topology_parameter, voltages.values, folder, add_to_path=f"_{thread}_f{f_mult}",
-            #                             save_th=1, T_val=0.0)
+                N                           = 5
+                voltages_values             = amplitude*np.cos(2*np.pi*frequency*time_steps)
+                topology_parameter          = {}
+                topology_parameter["Nx"]    = N
+                topology_parameter["Ny"]    = N
+                topology_parameter["Nz"]    = 1
+                # topology_parameter["e_pos"] = [[0,0,0], [int((N-1)/2),0,0], [N-1,0,0], [0,int((N-1)/2),0], [0,N-1,0], [N-1,int((N)/2),0], [int((N)/2),(N-1),0], [N-1,N-1,0]]
+                topology_parameter["e_pos"] = [[0,0,0], [N-1,N-1,0]]
+
+                target_electrode    = len(topology_parameter["e_pos"]) - 1
+                # folder              = f"/home/jonas/phd/NanoNets/test_runs/time_run/data/periodic/dis_f_01_"#"/scratch/tmp/j_mens07/data/system_size/"
+                folder              = f"/mnt/c/Users/jonas/Desktop/phd/NanoNets/test_runs/time_run/data/periodic/f={np.round(frequency)}_A={amplitude}"#"/scratch/tmp/j_mens07/data/system_size/"
+                voltages            = pd.DataFrame(np.zeros((N_voltages, N_electrodes+1)))
+                voltages.iloc[:,0]  = voltages_values
+                
+                np_info = {
+                    "eps_r"         : 2.6,
+                    "eps_s"         : 3.9,
+                    "mean_radius"   : 10.0,
+                    "std_radius"    : 0.0,
+                    "np_distance"   : 1.0
+                }
+
+                sim_class   = model.simulation(voltages.values)
+                sim_class.init_cubic(folder, topology_parameter, add_to_path=f"_{thread}_{inner_stat}", np_info=np_info)
+                sim_class.run_var_voltages(target_electrode, time_steps, save_th=1)
+                # model.time_simulation(target_electrode, time_steps, topology_parameter, voltages.values, folder, add_to_path=f"_{thread}_f{f_mult}",
+                #                             save_th=1, T_val=0.0)
 
     
     for i in range(N_processes):
 
-        process = multiprocessing.Process(target=parallel_code, args=(i, N_voltages, frequency, time_steps, amplitude, N_electrodes))
+        process = multiprocessing.Process(target=parallel_code, args=(i, N_voltages, time_steps, amplitude, N_electrodes))
         process.start()
--- scripts/time_dep_noise.py
+++ scripts/time_dep_noise.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("/home/jonas/phd/NanoNets/src/")
-# sys.path.append("/mnt/c/Users/jonas/Desktop/phd/NanoNets/src/")
+sys.path.append("/mnt/c/Users/jonas/Desktop/phd/NanoNets/src/")
 import model
 import multiprocessing
 
@@ -11,14 +11,18 @@ if __name__ == '__main__':
 
     N_processes         = 10
     N_electrodes        = 8
-    N_voltages          = 1000
-    max_time            = 2e-4
+    N_voltages          = 2500
+    max_time            = 10e-4
     time_steps          = np.linspace(0,max_time,N_voltages)
 
     def parallel_code(thread, N_voltages, time_steps, N_electrodes):
 
-        for inner_stat in range(10): 
-            N                           = 3
+        np.random.seed(thread)
+
+        for inner_stat in range(5):
+
+
+            N                           = 5
             min_val                     = -1
             max_val                     = 1
             data                        = np.cumsum(np.random.normal(0,1,N_voltages))
@@ -32,7 +36,8 @@ if __name__ == '__main__':
                                            [0,N-1,0], [N-1,int((N)/2),0], [int((N)/2),(N-1),0], [N-1,N-1,0]]
 
             target_electrode    = len(topology_parameter["e_pos"]) - 1
-            folder              = "/home/jonas/phd/NanoNets/test_runs/time_run/data/noise/"#"/scratch/tmp/j_mens07/data/system_size/"
+            # folder              = "/home/jonas/phd/NanoNets/test_runs/time_run/data/noise/"#"/scratch/tmp/j_mens07/data/system_size/"
+            folder              = "/mnt/c/Users/jonas/Desktop/phd/NanoNets/test_runs/time_run/data/noise/"#"/scratch/tmp/j_mens07/data/system_size/"
             voltages            = pd.DataFrame(np.zeros((N_voltages, N_electrodes+1)))
             voltages.iloc[:,0]  = voltages_values
 

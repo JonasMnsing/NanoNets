20633ec59c022487a85a37e0343abff5b89449e6
Jonas Mensing <jonasmensing@posteo.de>
2024-02-07 19:18:08 +0100

    memory cap

--- src/nanonets_utils.py
+++ src/nanonets_utils.py
@@ -44,3 +44,107 @@ def display_network(np_network_sim : nanonets.simulation, fig=None, ax=None, blu
             fig.savefig(save_to_path, bbox_inches='tight', transparent=True)
 
     return fig, ax
+
+def train_test_split(time, voltages, train_length, test_length, prediction_distance):
+
+    u_train = voltages[:train_length]
+    y_train = voltages[prediction_distance:(train_length+prediction_distance)]
+
+    u_test  = voltages[(train_length):(train_length+test_length)]
+    y_test  = voltages[(train_length+prediction_distance):(train_length+test_length+prediction_distance)]
+
+    t_train = time[:(train_length)]
+    t_test  = time[(train_length):(train_length+test_length)]
+
+    return t_train, u_train, y_train, t_test, u_test, y_test
+
+def train_test_split_memory(time, voltages, train_length, test_length, remember_distance, offset=100):
+
+    u_train = voltages[offset:(train_length+offset)]
+    y_train = voltages[(offset-remember_distance):(train_length+offset-remember_distance)]
+
+    u_test  = voltages[(train_length+offset):(train_length+offset+test_length)]
+    y_test  = voltages[(train_length+offset-remember_distance):(train_length+offset+test_length-remember_distance)]
+
+    t_train = time[offset:(train_length+offset)]
+    t_test  = time[(train_length+offset):(train_length+offset+test_length)]
+
+    return t_train, u_train, y_train, t_test, u_test, y_test
+
+def select_electrode_currents(np_network_sim : nanonets.simulation):
+
+    # Return Network Currents
+    jump_paths, network_I   = np_network_sim.return_network_currents()
+    network_I_df            = pd.DataFrame(network_I)
+    network_I_df.columns    = jump_paths
+    network_I_df            = network_I_df.reset_index(drop=True)
+
+    # Select Electrode Currents
+    electrode_currents  = pd.DataFrame()
+
+    for i in range(np_network_sim.N_electrodes):
+
+        for col in jump_paths:
+            if col[0] == i:
+                a = network_I_df[col]
+            if col[1] == i:
+                b = network_I_df[col]
+        
+        diff                        = b - a
+        electrode_currents[f'I{i}'] = diff
+
+    return electrode_currents
+
+def memory_capacity_simulation(time, voltages, train_length, test_length, remember_distance, network_topology, topology_parameter, np_info=None,
+                               save_th=10, folder='data/', regularization_coeff = 1e-8, path_info=''):
+
+    # Train Test Split
+    t_train, u_train, y_train, t_test, u_test, y_test = train_test_split_memory(time=time, voltages=voltages, train_length=train_length,
+                                                                                test_length=test_length, remember_distance=remember_distance)
+    
+    # Run Train Simulation
+    np_network_sim = nanonets.simulation(network_topology=network_topology, topology_parameter=topology_parameter, np_info=np_info,
+                                         folder=folder, add_to_path=f'_mc_train_{remember_distance}{path_info}')
+    np_network_sim.run_var_voltages(voltages=u_train, time_steps=t_train, target_electrode=(np_network_sim.N_electrodes-1), save_th=save_th)
+
+    # Return Electrode Currents
+    electrode_currents      = select_electrode_currents(np_network_sim)
+    electrode_currents['t'] = t_train[1:]
+    electrode_currents['U'] = u_train[1:,0]
+
+    # Save Train Electrode Currents 
+    electrode_currents.to_csv(f"{folder}mc_train_results_{remember_distance}{path_info}.csv")
+
+    # Regression of Electrode Currents and Train Target
+    X       = electrode_currents.loc[:,'I1':f'I{np_network_sim.N_electrodes-1}'].T.values
+    y       = y_train[1:,0].copy()
+    W_out   = np.linalg.solve(np.dot(X,X.T) + regularization_coeff*np.eye(X.shape[0]), np.dot(X,y.T)).T
+
+    # Store Wout Matrix
+    np.savetxt(f"{folder}Wout_{remember_distance}{path_info}.csv", W_out)
+
+    # Run Test Simulation
+    np_network_sim = nanonets.simulation(network_topology=network_topology, topology_parameter=topology_parameter, np_info=np_info,
+                                         folder=folder, add_to_path=f'_mc_test_{remember_distance}{path_info}')
+    np_network_sim.run_var_voltages(voltages=u_test, time_steps=t_test, target_electrode=(np_network_sim.N_electrodes-1), save_th=save_th)
+
+    # Return Electrode Currents
+    electrode_currents      = select_electrode_currents(np_network_sim)
+    electrode_currents['t'] = t_test[1:]
+    electrode_currents['U'] = u_test[1:,0]
+    X                       = electrode_currents.loc[:,'I1':f'I{np_network_sim.N_electrodes-1}'].values
+
+    # Predict y_test based on W_out
+    y_pred  = []
+
+    for val in X:
+
+        y_val   = np.dot(W_out,val[:,np.newaxis])[0]
+        y_pred.append(y_val)
+
+    electrode_currents['y_test'] = y_test[1:,0]
+    electrode_currents['y_pred'] = np.array(y_pred)
+    
+    # Save Test Electrode Currents 
+    electrode_currents.to_csv(f"{folder}mc_test_results_{remember_distance}{path_info}.csv")
+    

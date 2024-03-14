f560c55e99e9189d169f638a57610fca5728eb34
Jonas Mensing <jonasmensing@posteo.de>
2024-03-14 23:35:58 +0100

    nanonets sim stat

--- src/nanonets.py
+++ src/nanonets.py
@@ -552,7 +552,17 @@ class model_class():
                 counter = 0
         
         return self.total_jumps
+    
+    def return_next_means(self, new_value, mean_value, mean_value2):
 
+        delta           =   new_value - mean_value
+        mean_value      +=  delta/self.total_jumps           
+        delta2          =   new_value - mean_value
+        mean_value2     +=  delta * delta2
+
+        return mean_value, mean_value2
+
+    
     def kmc_simulation(self, target_electrode : int, error_th = 0.05, max_jumps=10000000):
         """
         Runs KMC until current for target electrode has a relative error below error_th or max_jumps is reached
@@ -650,7 +660,7 @@ class model_class():
 
         self.jump_storage = self.jump_storage/self.time
     
-    def kmc_simulation_fixed(self, target_electrode : int, n_stat=10, max_jumps=1000000):
+    def kmc_simulation_fixed(self, target_electrode : int, error_th = 0.05, max_jumps=1000000):
         """
         Runs KMC until current for target electrode has a relative error below error_th or max_jumps is reached
         Tracks Mean Current, Current standard deviation, average microstate (charge vector), contribution of each junction
@@ -665,70 +675,72 @@ class model_class():
             maximum number of jumps before function ends
         """
         
-        self.total_jumps                = max_jumps
-        jump_diff_vals                  = np.zeros(n_stat)
-        np1, np2                        = self.adv_index_cols[self.jump], self.adv_index_rows[self.jump]     
-        self.calc_potentials()
+        N_calculations                  = 0
+        self.total_jumps                = 0
+        self.time                       = 0.0
+        self.rel_error                  = 1.0
+        self.counter_output_jumps_pos   = 0
+        self.counter_output_jumps_neg   = 0
+        self.jump_diff_mean             = 0.0
+        self.jump_diff_mean2            = 0.0
+        self.jump_diff_std              = 0.0
+        np1, np2                        = self.adv_index_cols[self.jump], self.adv_index_rows[self.jump]   
+        self.calc_potentials()        
 
-        for i in range(n_stat):
-
-            self.time                       = 0.0
-            self.counter_output_jumps_pos   = 0
-            self.counter_output_jumps_neg   = 0
-            self.jump_diff_mean             = 0.0
-            self.jump_diff_std              = 0.0
-
-            for j in range(self.total_jumps):
-
-            # if ((self.total_jumps == max_jumps) or (self.jump == -1)):
-
-            #     break
+        while(self.rel_error > error_th):
+            
+            if ((self.total_jumps == max_jumps) or (self.jump == -1)):
+                break
+                
+            if (self.total_jumps % 1000 == 0):
+                pass
                         
-                # KMC Part
-                random_number1  = np.random.rand()
-                random_number2  = np.random.rand()
+            # KMC Part
+            random_number1  = np.random.rand()
+            random_number2  = np.random.rand()
 
-                if self.cotunneling == False:
-                    if not(self.zero_T):
-                        self.calc_tunnel_rates()
-                    else:
-                        self.calc_tunnel_rates_zero_T()
-                    self.select_event(random_number1, random_number2)
+            if self.cotunneling == False:
 
+                if not(self.zero_T):
+                    self.calc_tunnel_rates()
+                else:
+                    self.calc_tunnel_rates_zero_T()
+                self.select_event(random_number1, random_number2)
+
+                np1 = self.adv_index_rows[self.jump]
+                np2 = self.adv_index_cols[self.jump]
+                self.jump_storage[self.jump]    += 1
+
+            else:
+                
+                if not(self.zero_T):
+                    self.calc_tunnel_rates()
+                    self.calc_cotunnel_rates()
+                else:
+                    self.calc_tunnel_rates_zero_T()
+                    self.calc_cotunnel_rates_zero_T()
+
+                self.select_co_event(random_number1, random_number2)
+
+                if self.co_event_selected:
+                    np1 = self.co_adv_index1[self.jump]
+                    np2 = self.co_adv_index3[self.jump]
+                    self.jump_storage_co[self.jump] += 1
+                else:
                     np1 = self.adv_index_rows[self.jump]
                     np2 = self.adv_index_cols[self.jump]
                     self.jump_storage[self.jump]    += 1
 
-                else:
-                    
-                    if not(self.zero_T):
-                        self.calc_tunnel_rates()
-                        self.calc_cotunnel_rates()
-                    else:
-                        self.calc_tunnel_rates_zero_T()
-                        self.calc_cotunnel_rates_zero_T()
+            # self.charge_mean    += self.charge_vector
+            # self.potential_mean += self.potential_vector
 
-                    self.select_co_event(random_number1, random_number2)
-
-                    if self.co_event_selected:
-                        np1 = self.co_adv_index1[self.jump]
-                        np2 = self.co_adv_index3[self.jump]
-                        self.jump_storage_co[self.jump] += 1
-                    else:
-                        np1 = self.adv_index_rows[self.jump]
-                        np2 = self.adv_index_cols[self.jump]
-                        self.jump_storage[self.jump]    += 1
-
-                # self.charge_mean    += self.charge_vector
-                # self.potential_mean += self.potential_vector
-
-                # If jump from target electrode
-                if (np1 == target_electrode):
-                    self.counter_output_jumps_neg += 1
-                    
-                # If jump towards target electrode
-                if (np2 == target_electrode):
-                    self.counter_output_jumps_pos += 1
+            # If jump from target electrode
+            if (np1 == target_electrode):
+                self.counter_output_jumps_neg += 1
+                
+            # If jump towards target electrode
+            if (np2 == target_electrode):
+                self.counter_output_jumps_pos += 1
             
             jump_diff_vals[i] = (self.counter_output_jumps_pos - self.counter_output_jumps_neg)/self.time
         

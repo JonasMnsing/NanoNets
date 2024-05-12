e9a685559598ac5ff72f11f0dc1c2a12377e89cf
Jonas Mensing <jonasmensing@posteo.de>
2024-05-12 21:39:04 +0200

    vortrag

--- scripts/2_funding_period/WP2/step_input/step_input_resistance.py
+++ scripts/2_funding_period/WP2/step_input/step_input_resistance.py
@@ -16,14 +16,14 @@ import nanonets_utils
 import multiprocessing
 
 # Simulation Function
-def parallel_code(thread, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder, np_info, T_val, save_th, stat_size):
+def parallel_code(thread, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder, np_info, T_val, save_th, stat_size, seed):
 
     target_electrode    = len(topology_parameter["e_pos"]) - 1
     R_val               = res_info2["R"]
     
     for s in range(stat_size):
 
-        sim_class = nanonets.simulation(network_topology='cubic', topology_parameter=topology_parameter, folder=folder+f"data/R_{R_val}/r1/", res_info=res_info, res_info2=res_info2, np_info=np_info, add_to_path=f'_t{thread}_s{s}')
+        sim_class = nanonets.simulation(network_topology='cubic', topology_parameter=topology_parameter, folder=folder+f"data/R_{R_val}/r{seed}/", res_info=res_info, res_info2=res_info2, np_info=np_info, add_to_path=f'_t{thread}_s{s}')
         sim_class.run_var_voltages(voltages=voltages, time_steps=time_steps, target_electrode=target_electrode, eq_steps=eq_steps, T_val=T_val, save_th=save_th)
 
 if __name__ == '__main__':
@@ -31,48 +31,65 @@ if __name__ == '__main__':
     folder      = "scripts/2_funding_period/WP2/step_input/1I_1O_R_dis/"
     voltages    = np.loadtxt(folder+'volt.csv')
     time_steps  = np.loadtxt(folder+'time.csv')
-    stat_size   = 50
+    stat_size   = 10
 
-    N_processes, network_topology, topology_parameter, eq_steps, np_info, res_info, T_val, save_th = nanonets_utils.load_time_params(folder=folder)
+    for seed in range(1,4):
 
-    res_info2   = {
-        "R" : 50,
-        "np_index" : np.random.choice(np.arange(1,48), 9, replace=False)
-    }
+        rs = np.random.RandomState(seed)
 
-    procs = []
-    for i in range(N_processes):
+        N_processes, network_topology, topology_parameter, eq_steps, np_info, res_info, T_val, save_th = nanonets_utils.load_time_params(folder=folder)
 
-        process = multiprocessing.Process(target=parallel_code, args=(i, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder,
-                                                                      np_info, T_val, save_th, stat_size))
-        process.start()
-        procs.append(process)
-    
-    for p in procs:
-        p.join()
+        res_info2   = {
+            "R" : 50,
+            "np_index" : rs.choice(np.arange(1,48), 9, replace=False)
+        }
 
-    res_info2["R"] = 100
+        procs = []
+        for i in range(N_processes):
 
-    procs = []
-    for i in range(N_processes):
+            process = multiprocessing.Process(target=parallel_code, args=(i, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder,
+                                                                        np_info, T_val, save_th, stat_size, seed))
+            process.start()
+            procs.append(process)
+        
+        for p in procs:
+            p.join()
 
-        process = multiprocessing.Process(target=parallel_code, args=(i, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder,
-                                                                      np_info, T_val, save_th, stat_size))
-        process.start()
-        procs.append(process)
-    
-    for p in procs:
-        p.join()
+        res_info2["R"] = 100
 
-    res_info2["R"] = 200
+        procs = []
+        for i in range(N_processes):
 
-    procs = []
-    for i in range(N_processes):
+            process = multiprocessing.Process(target=parallel_code, args=(i, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder,
+                                                                        np_info, T_val, save_th, stat_size, seed))
+            process.start()
+            procs.append(process)
+        
+        for p in procs:
+            p.join()
 
-        process = multiprocessing.Process(target=parallel_code, args=(i, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder,
-                                                                      np_info, T_val, save_th, stat_size))
-        process.start()
-        procs.append(process)
-    
-    for p in procs:
-        p.join()
+        res_info2["R"] = 200
+
+        procs = []
+        for i in range(N_processes):
+
+            process = multiprocessing.Process(target=parallel_code, args=(i, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder,
+                                                                        np_info, T_val, save_th, stat_size, seed))
+            process.start()
+            procs.append(process)
+        
+        for p in procs:
+            p.join()
+
+        res_info2["R"] = 400
+
+        procs = []
+        for i in range(N_processes):
+
+            process = multiprocessing.Process(target=parallel_code, args=(i, voltages, time_steps, topology_parameter, res_info, res_info2, eq_steps, folder,
+                                                                        np_info, T_val, save_th, stat_size, seed))
+            process.start()
+            procs.append(process)
+        
+        for p in procs:
+            p.join()

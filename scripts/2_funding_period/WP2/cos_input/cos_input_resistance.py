93a4f99185ae751e8aaf4c90472885e5493e490c
jonas@work <j_mens07@uni-muenster.de>
2024-09-18 16:00:59 +0200

    cosine input

--- scripts/2_funding_period/WP2/cos_input/cos_input_resistance.py
+++ scripts/2_funding_period/WP2/cos_input/cos_input_resistance.py
@@ -6,19 +6,24 @@ sys.path.append("/home/jonas/phd/NanoNets/src/")
 sys.path.append("/mnt/c/Users/jonas/Desktop/phd/NanoNets/src/")
 
 import nanonets
+import multiprocessing
 
 # Simulation Function
-def run_simulation(voltages, time_steps, network_topology, topology_parameter, eq_steps, folder, stat_size, seed, freq):
+def run_simulation(freq, time_steps, network_topology, topology_parameter, eq_steps, folder, stat_size, seed, amplitude, N_voltages, R_val, res_info2):
 
+    voltages        = np.zeros(shape=(N_voltages,3))
+    voltages[:,0]   = amplitude*np.cos(freq*time_steps*1e8)
+    
     target_electrode = len(topology_parameter["e_pos"]) - 1
     
-    sim_class = nanonets.simulation(network_topology=network_topology, topology_parameter=topology_parameter, folder=folder+"data/", seed=seed, add_to_path=f'_{np.round(freq,2)}')
+    sim_class = nanonets.simulation(network_topology=network_topology, topology_parameter=topology_parameter, folder=folder+"data/",
+                                    seed=seed, add_to_path=f'_{np.round(freq,2)}_{R_val}_{seed}', res_info2=res_info2)
     sim_class.run_var_voltages(voltages=voltages, time_steps=time_steps, target_electrode=target_electrode, eq_steps=eq_steps, stat_size=stat_size)
 
 if __name__ == '__main__':
 
     # Parameter
-    folder              = "scripts/2_funding_period/WP2/cos_input/uniform/"
+    folder              = "scripts/2_funding_period/WP2/cos_input/resistance/"
     stat_size           = 1000
     eq_steps            = 1000000
     network_topology    = "cubic"
@@ -38,13 +43,41 @@ if __name__ == '__main__':
     N_voltages  = 10000
     time        = step_size*np.arange(N_voltages)
     amplitude   = 0.2
-    
-    # Voltages
-    voltages    = np.zeros(shape=(N_voltages,3))
 
-    for freq in np.arange(0.1,4,0.2):
+    for R_val in [50,100,200,400,800]:
 
-        # Input Electrode
-        voltages[:,0] = amplitude*np.cos(freq*time*1e8)
+        for seed in range(10):
 
-        run_simulation(voltages, time, network_topology, topology_parameter, eq_steps, folder, stat_size, seed, freq)
+            rs          = np.random.RandomState(seed=seed)
+            res_info2   = {
+                "R"         : R_val,
+                "np_index"  : rs.choice(np.arange(1,48), 9, replace=False)
+            }
+
+            # Parameter
+            frequencies = np.arange(0.1,2,0.2)
+            N_processes = len(frequencies)
+
+            procs = []
+            for i in range(N_processes):
+
+                process = multiprocessing.Process(target=run_simulation, args=(frequencies[i], time, network_topology, topology_parameter,
+                                                                            eq_steps, folder, stat_size, seed, amplitude, N_voltages, R_val, res_info2))
+                process.start()
+                procs.append(process)
+            
+            for p in procs:
+                p.join()
+
+            frequencies = np.arange(2.1,4,0.2)
+
+            procs = []
+            for i in range(N_processes):
+
+                process = multiprocessing.Process(target=run_simulation, args=(frequencies[i], time, network_topology, topology_parameter,
+                                                                            eq_steps, folder, stat_size, seed, amplitude, N_voltages, R_val, res_info2))
+                process.start()
+                procs.append(process)
+            
+            for p in procs:
+                p.join()

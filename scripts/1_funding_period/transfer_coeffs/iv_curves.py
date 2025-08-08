e8988d4bc2aafc0fa684a8a8673e2becf01ea2d6
Jonas Mensing <jonasmensing@posteo.de>
2025-08-08 09:50:35 +0200

    transfer_coeff

---
+++ scripts/1_funding_period/transfer_coeffs/iv_curves.py
@@ -0,0 +1,48 @@
+import logging
+import numpy as np
+from pathlib import Path
+
+# NanoNets
+from nanonets.utils import run_static_simulation, batch_launch
+
+# ─── Configuration ───
+N_MIN, N_MAX    = 3, 16
+N_E             = 8
+V_MIN, V_MAX    = 0.0, 1.0
+N_DATA          = 100
+N_PROCS         = 10
+T_VAL           = 5
+LOG_LEVEL       = logging.INFO
+PATH            = Path("/mnt/c/Users/jonas/Desktop/phd/data/1_funding_period/electrode_impact/voltage_sweep/")
+INPUT_POS       = [0,1,2,5]
+# ---------------------
+
+def main():
+    logging.basicConfig(level=LOG_LEVEL, format="%(asctime)s %(levelname)s: %(message)s")
+    PATH.mkdir(parents=True, exist_ok=True)
+    tasks = []
+    for n in range(N_MIN, N_MAX+1):
+        topo = {"Nx": n, "Ny": n,
+                "e_pos" : [[0,0], 
+                           [int((n-1)/2),0],
+                           [n-1,0], 
+                           [0,int((n-1)/2)],
+                           [0,n-1],
+                           [n-1,int((n)/2)],
+                           [int((n)/2),(n-1)],
+                           [n-1,n-1]],
+                "electrode_type" : ['constant']*N_E}
+        for pos in INPUT_POS:
+            volt        = np.zeros(shape=(N_DATA,N_E+1))
+            volt[:,pos] = np.linspace(V_MIN, V_MAX, N_DATA)
+            args        = (volt,topo,PATH)
+            kwargs      = {
+                'net_kwargs':{'add_to_path':f'_{pos}_T_{T_VAL}'},
+                'sim_kwargs':{'save_th':10, 'T_val':T_VAL},
+            }
+            tasks.append((args,kwargs))
+    
+    batch_launch(run_static_simulation, tasks, N_PROCS)
+        
+if __name__ == "__main__":
+    main()

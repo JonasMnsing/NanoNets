ed82452aeac8762ac0b3fad12d75b804b279464c
jonasmensing <j_mens07@uni-muenster.de>
2025-08-19 16:22:29 +0200

    time constant

--- scripts/2_funding_period/WP2/magic_cable/1_DC_vs_cap.py
+++ scripts/2_funding_period/WP2/magic_cable/1_DC_vs_cap.py
@@ -9,8 +9,8 @@ STAT_SIZE   = 50
 N_VOLT      = 200000
 TIME_STEP   = 1e-10
 N_P         = 9
-# FOLDER      = Path("/home/j/j_mens07/phd/data/2_funding_period/")
-FOLDER      = Path("/mnt/c/Users/jonas/Desktop/phd/data/2_funding_period/")
+FOLDER      = Path("/home/j/j_mens07/phd/data/2_funding_period/")
+# FOLDER      = Path("/mnt/c/Users/jonas/Desktop/phd/data/2_funding_period/")
 CAP_VALS    = [1e1, 5e1, 1e2, 5e2, 1e3, 5e3, 1e4, 5e4, 1e5, 5e5, 1e6]
 CPU_CNT     = len(CAP_VALS)
 LOG_LEVEL   = logging.INFO
@@ -54,27 +54,27 @@ def main():
         tasks.append((args, kwargs))
 
     # Eight-electrode (constant at 7, floating last)
-    # topo8 = {
-    #     "Nx": N_P,
-    #     "Ny": N_P,
-    #     "e_pos": [[(N_P-1)//2,0],[0,0],[N_P-1,0],
-    #                [0,(N_P-1)//2],[N_P-1,(N_P-1)//2],
-    #                [0,N_P-1],[N_P-1,N_P-1],[(N_P-1)//2,N_P-1]],
-    #     "electrode_type": ['constant']*7 + ['floating']
-    # }
-    # volt8       = np.zeros((N_VOLT, len(topo8['e_pos'])+1), float)
-    # volt8[:, 0] = U_0
+    topo8 = {
+        "Nx": N_P,
+        "Ny": N_P,
+        "e_pos": [[(N_P-1)//2,0],[0,0],[N_P-1,0],
+                   [0,(N_P-1)//2],[N_P-1,(N_P-1)//2],
+                   [0,N_P-1],[N_P-1,N_P-1],[(N_P-1)//2,N_P-1]],
+        "electrode_type": ['constant']*7 + ['floating']
+    }
+    volt8       = np.zeros((N_VOLT, len(topo8['e_pos'])+1), float)
+    volt8[:, 0] = U_0
 
-    # for cap in CAP_VALS:
-    #     args    = (time_steps, volt8, topo8, out_base)
-    #     kwargs  = {'net_kwargs': {'high_C_output': True,
-    #                              'np_info2': {'np_index': [81],
-    #                                           'mean_radius': cap,
-    #                                           'std_radius': 0.0},
-    #                              'pack_optimizer':False,
-    #                              'add_to_path': f"_{cap}"},
-    #                 'sim_kwargs': {'T_val':T_VAL,'stat_size':STAT_SIZE,'save':True}}
-    #     tasks.append((args, kwargs))
+    for cap in CAP_VALS:
+        args    = (time_steps, volt8, topo8, out_base)
+        kwargs  = {'net_kwargs': {'high_C_output': True,
+                                 'np_info2': {'np_index': [81],
+                                              'mean_radius': cap,
+                                              'std_radius': 0.0},
+                                 'pack_optimizer':False,
+                                 'add_to_path': f"_{cap}"},
+                    'sim_kwargs': {'T_val':T_VAL,'stat_size':STAT_SIZE,'save':True}}
+        tasks.append((args, kwargs))
 
     # Launch all simulations in parallel
     batch_launch(run_dynamic_simulation, tasks, CPU_CNT)

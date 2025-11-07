2d3b0eb7d7bdf1327644d32f23420283b1c2e735
Jonas Mensing <jonasmensing@posteo.de>
2025-11-07 13:51:38 +0100

    relaxation

--- scripts/2_funding_period/WP2/DC/relaxation.py
+++ scripts/2_funding_period/WP2/DC/relaxation.py
@@ -3,18 +3,16 @@ import logging, numpy as np
 from nanonets.utils import batch_launch, run_dynamic_simulation
 
 # ─── Configuration ────────────────────────────────────────────────────────────────
-T_VAL        = 5.0
 N_VOLTAGES   = 50000
-TIME_STEP    = 1e-10
-STAT_SIZE    = 200
+TIME_STEP    = 1e-11
+STAT_SIZE    = 1000
 N_P          = 9
-U0_LIST      = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+CPU_CNT      = 32
+U0_LIST      = np.linspace(0.01,0.05,CPU_CNT,endpoint=False)
 OUTPUT_DIR   = Path("/home/j/j_mens07/phd/data/2_funding_period/")
 # BASE_DIR   = Path("/mnt/c/Users/jonas/Desktop/phd/data/2_funding_period/")
 LOG_LEVEL    = logging.INFO
-CPU_CNT      = 10
 # ────────────────────────────────────────────────────────────────────────────────
-
 
 def main():
     logging.basicConfig(level=LOG_LEVEL,
@@ -26,28 +24,23 @@ def main():
     out_base = OUTPUT_DIR / "potential/wo_magic_cable/dc_input_vs_volt"
     out_base.mkdir(parents=True, exist_ok=True)
 
-    # --- 2‐electrode (one constant, one floating) setup ---
-    topo_2 = {"Nx": N_P,"Ny": N_P,
-              "e_pos": [[(N_P-1)//2, 0],[(N_P-1)//2, N_P-1]],
-              "electrode_type": ['constant', 'floating']}
-    # --- 8‐electrode (7 constant, 1 floating) setup ---
-    topo_8 = {"Nx": N_P,"Ny": N_P,"Nz": 1,
+    # --- 8‐electrode setup ---
+    topo = {"Nx": N_P,"Ny": N_P,"Nz": 1,
                 "e_pos": [[(N_P-1)//2, 0],[0, 0],[N_P-1, 0],
                         [0, (N_P-1)//2],[N_P-1, (N_P-1)//2],
                         [0, N_P-1],[N_P-1, N_P-1],[(N_P-1)//2, N_P-1]],
-                "electrode_type": ['constant']*7 + ['floating']}
-    for topo in [topo_2, topo_8]:
-        n_elec = len(topo["e_pos"])
-        for U0 in U0_LIST:
-            volt        = np.zeros((N_VOLTAGES, n_elec+1), float)
-            volt[:, 0]  = U0
-            out_base.mkdir(exist_ok=True)
-            args    = (time_pts, volt, topo, out_base)
-            kwargs  = {
-                'net_kwargs': {'add_to_path' : f"_{U0:.3f}"},
-                'sim_kwargs': {'T_val':T_VAL,'stat_size':STAT_SIZE,'save':True}
-            }
-            tasks.append((args, kwargs))
+                "electrode_type": ['constant']*8}
+    n_elec = len(topo["e_pos"])
+    for U0 in U0_LIST:
+        volt        = np.zeros((N_VOLTAGES, n_elec+1), float)
+        volt[:, 0]  = U0
+        out_base.mkdir(exist_ok=True)
+        args    = (time_pts, volt, topo, out_base)
+        kwargs  = {
+            'net_kwargs': {'add_to_path' : f"_{U0:.3f}"},
+            'sim_kwargs': {'stat_size':STAT_SIZE,'save':True}
+        }
+        tasks.append((args, kwargs))
 
     batch_launch(run_dynamic_simulation, tasks, CPU_CNT)
 

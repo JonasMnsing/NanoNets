1150d8759f1aa93f23b6d4aec5a59b9e4f3a4564
jonasmensing <j_mens07@uni-muenster.de>
2025-12-16 09:52:28 +0100

    .

--- scripts/1_funding_period/iv_curves/network_iv.py
+++ scripts/1_funding_period/iv_curves/network_iv.py
@@ -14,12 +14,13 @@ N_INPUTS        = 100
 N_GATES         = 960
 V_INPUT         = np.round(np.linspace(-V_INPUT_MAX, V_INPUT_MAX, N_INPUTS),4)
 # V_INPUT         = np.round(np.linspace(-V_INPUT_MAX, V_INPUT_MAX, N_INPUTS),4)
-V_GATES         = [0.0] #np.round(np.linspace(-V_GATE_MAX, V_GATE_MAX, N_INPUTS),4)
+V_GATES         = [0.0] # np.round(np.linspace(-V_GATE_MAX, V_GATE_MAX, N_INPUTS),4)
 LOG_LEVEL       = logging.INFO
-PATH            = Path("/mnt/c/Users/jonas/Desktop/phd/nanonets/scripts/1_funding_period/iv_curves/net/data/")
+# PATH            = Path("/mnt/c/Users/jonas/Desktop/phd/nanonets/scripts/1_funding_period/iv_curves/net/data/")
+PATH            = Path("/home/j/j_mens07/phd/NanoNets/scripts/1_funding_period/iv_curves/net/data/")
 topo            = {"Nx": N_NP, "Ny": N_NP, "e_pos" : [[0,0], [int((N_NP-1)/2),0], [N_NP-1,0], [0,int((N_NP-1)/2)],
                        [0,N_NP-1], [N_NP-1,int((N_NP)/2)], [int((N_NP)/2),(N_NP-1)], [N_NP-1,N_NP-1]], "electrode_type": ['constant']*8}
-# sim_dic         = {"duration" : True, "error_th" : 0.05, "max_jumps" : 10000000, "n_eq" : 500, "n_per_batch" : 50, "kmc_counting" : False, "min_batches" : 5}
+# sim_dic         = {"duration" : True, "error_th" : 0.05, "max_jumps" : 10000000, "n_eq" : 10000, "n_per_batch" : 1000, "kmc_counting" : False, "min_batches" : 5}
 sim_dic         = {"duration" : False, "error_th" : 0.05, "max_jumps" : 10000000, "n_eq" : 100000, "n_per_batch" : 2000, "kmc_counting" : False, "min_batches" : 5}
 # --------------------- 
 
